d9521fe70bb549a8799896bcdae0990aae6df26b
nandor matyas <nandor.matyas@gmail.com>
2019-01-30 12:01:06 +0100

    feat(cron): app checks ip every 30mins

--- ipMailer.py
+++ ipMailer.py
@@ -1,22 +1,33 @@
 import smtplib, ssl
 import ipGetter
 import env
+import schedule
+import time
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = env.os.environ['SENDER']
 receiver_email = env.os.environ['RECEIEVER']
 password = env.os.environ['PASS']
-message2 = ipGetter.ipGetter()
 
-if message2 :
-    print("Ip changed, preparing email...")
-    message = "Subject: {} \n\n Your IP has changed to {}".format("Your home server IP changed", message2)
+def mailer():
 
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
-        print("Email sent")
-else :
-    print("Ip did not change")
+    message2 = ipGetter.ipGetter()
+
+    if message2 :
+        print("Ip changed, preparing email...")
+        message = "Subject: {} \n\n Your IP has changed to {}".format("Your home server IP changed", message2)
+
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
+            print("Email sent")
+    else :
+        print("Ip did not change")
+
+schedule.every(30).minutes.do(mailer)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
