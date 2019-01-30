4803d2c6e1f08ac5522f6c4343381e23c56cf5fd
nandor matyas <nandor.matyas@gmail.com>
2019-01-30 13:07:14 +0100

    feat(logs): logging added

--- fileController.py
+++ fileController.py
@@ -4,7 +4,7 @@ def ipChecker(ip):
     if os.path.exists("./ip.txt"):
         ipFile = open("ip.txt","r+")
         last_ip = ipFile.readlines()
-        if last_ip[-1] == ip :
+        if last_ip[-1] == ip:
             ipFile.close()
             return False
         else : 
@@ -13,5 +13,19 @@ def ipChecker(ip):
             return True
     else :
         ipFile = open("ip.txt","w+")
-        print('File created')
+        ipFile.write(ip)
+        print("ipFile created")
+        ipFile.close()
     
+def logCreator(time, message):
+    if os.path.exists("./logs.txt"):
+        logFile = open("logs.txt","r+")
+        logFile.readlines()
+        logFile.write("\n" + time + " " + message)
+        logFile.close()
+    else :
+        logFile = open("logs.txt","w+")
+        logFile.write(time + " " + message)
+        print("logFile created")
+        logFile.close()
+
