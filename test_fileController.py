c34bf1fd73d7e68724d6a95c7f8ffa9729ace307
nandor matyas <nandor.matyas@gmail.com>
2019-01-31 12:22:31 +0100

    feat(testing): fixes

--- test_fileController.py
+++ test_fileController.py
@@ -3,8 +3,13 @@ import random
 
 ip = str(random.randint(1, 20))
 
-def test_ipCheker():
-    assert fileController.ipChecker(ip) == True
+def test_ipCheker_create():
+    assert fileController.ipChecker(ip) == None
 
+    
 def test_already_saved():
-    assert fileController.ipChecker(ip) == False    
+    assert fileController.ipChecker(ip) == False
+
+def test_ipChecker_new():
+    ip_new = str(random.randint(21, 40))
+    assert fileController.ipChecker(ip_new) == True
